86d934cdc165a8812dc1e601ca83a623f05d7bea
Canul <canuladriana24@gmail.com>
2024-05-15 14:24:51 -0600

    tercer commit

--- app/src/main/java/com/example/listplanetasapp/MainActivity.java
+++ app/src/main/java/com/example/listplanetasapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.listplanetasapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ImageView imagen;
+    TextView descripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        spinner = findViewById(R.id.spinner);
+        imagen = findViewById(R.id.imagenPlaneta);
+        descripcion = findViewById(R.id.descripcionPlanetas);
+
+        String[] nombresdeplanetas = getResources().getStringArray(R.array.planetas_array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nombresdeplanetas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int imagenid = getResources().getIdentifier(nombresdeplanetas[position].toLowerCase(), "drawable", getPackageName());
+                imagen.setImageResource(imagenid);
+
+                String[] planetaDescripcion = getResources().getStringArray(R.array.desripcion_planetas);
+                descripcion.setText(planetaDescripcion[position]);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                descripcion.setText("No has seleccionado ningún planeta");
+            }
         });
+
     }
 }
